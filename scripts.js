49a4eaf62c7f582cfb0a1d7f152b9ace99852de8
anthony paiva <adpaguero1992@gmail.com>
2023-04-26 16:27:48 -0500

    primer commit de mi proyecto el ecomerce, eh adelantado la maquetacion y realize una funcion con javascript en el header para mostar y ocultar, eh dado estilos con css a la mayor parte de la pagina

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const mostrar = document.getElementById('listaOculta');
+
+
+function mostrarLista () {
+    if( mostrar.style.display === "none"){
+    mostrar.style.display="block"
+    }else{
+        mostrar.style.display="none"
+    }
+}
+
